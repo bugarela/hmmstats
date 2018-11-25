1a510353853b19cb0265818dfc0a08f198889888
Gabriela Moreira <gabriela@magrathealabs.com>
2018-11-25 18:30:42 -0200

    :ambulance: fix origin issues

--- hmmstats/app.py
+++ hmmstats/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 import json
 import pandas as pd
@@ -8,6 +9,7 @@ from hmmstats.resources.media_minuto import MediaMinutoResource
 from hmmstats.resources.tops import TopsResource
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 api.add_resource(MediaMinutoResource, '/media_minuto')
